7caeb1b9469572b303258e0fb73f994abc6b1e60
Zayelion <panthrowzay@gmail.com>
2016-04-10 03:02:34 -0400

    Working Login

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+/*global $, console*/
+
+
 function pagenavto(target) {
     'use strict';
     $('.activescreen').removeClass('activescreen');
-    $('section').css('left', '100vw');
-    $('#' + target).css('left', '0').addClass('activescreen');
+    $('section').css('display', 'none');
+    $('#' + target).css('display', 'block').addClass('activescreen');
     return false;
 }
+
+$('#formLogin').submit(function (event) {
+    'use strict';
+    var url = "http://www.duelingnetwork.com:8080/Dueling_Network/v2/action/login",
+        input = $(this).serialize(); // this refers to $('#formLogin')-> result.
+
+    $.post(url, input, function (data) {
+        console.log(data);
+        if (data.success) {
+            pagenavto('mainscreen');
+        }
+    });
+
+    event.preventDefault();
+});
